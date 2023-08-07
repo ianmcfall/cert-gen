da7767199765acfcfc302ba2e7296d7ee1b762cb
TalhaAnis <t.anis60.ta@gmail.com>
2023-08-07 16:55:45 +0500

    Updated deployment file

--- single-server.sh
+++ single-server.sh
@@ -36,7 +36,7 @@ read
 
 if ! command -v go &>/dev/null; then 
     wget https://go.dev/dl/go1.20.6.linux-amd64.tar.gz
-    rm -rf /usr/local/go && tar -C /usr/local -xzf go1.20.6.linux-amd64.tar.gz
+    sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf go1.20.6.linux-amd64.tar.gz
     rm go1.20.6.linux-amd64.tar.gz
     echo "export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin" >> $HOME/.profile
     source $HOME/.profile
